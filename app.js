7c5fbb6cdd0b865d0c279f94de4ae90e93d505b9
anushkxa <anushka082006@gmail.com>
2025-08-22 02:08:20 +0530

    template styling

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const path=require("path");
+const ejsMate= require("ejs-mate");
 const mongoose = require("mongoose");
 const Listing = require("./models/lisiting");
 const methodOverride= require("method-override");
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"/public")));
 
 async function main(){
     try {
