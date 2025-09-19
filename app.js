f9255bdc18659d568748cb97c347a9a1f4f551eb
anushkxa <anushka082006@gmail.com>
2025-09-19 18:23:53 +0530

    review through populate

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.get("/listings/new", (req,res)=>{
 //will show  all info of specific hotel hheehe
 app.get("/listings/:id", async(req,res)=>{
     let {id}= req.params;
-    const listing=await Listing.findById(id);
+    const listing=await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listing});
 
 })
